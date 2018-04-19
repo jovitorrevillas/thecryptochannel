5337165657c31efac62eb2abb8e7a39cc3e95f2b
peterbrionibinex <peter.br@ibinex.tech>
2018-04-19 15:09:32 +0800

    test1

--- wp-content/themes/pennews-child/single.php
+++ wp-content/themes/pennews-child/single.php
@@ -11,6 +11,9 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" >
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+			<?php  ?>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php
