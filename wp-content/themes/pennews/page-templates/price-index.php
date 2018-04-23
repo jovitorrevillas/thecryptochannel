862760a259b5a440c3712fbdad7fb4b55b2946ee
Leo Ordonez III <lordonez@ad.acquireap>
2018-04-23 15:45:08 +0800

    added new page template for crypto price index pages

---
+++ wp-content/themes/pennews/page-templates/price-index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Price Index Template
+ * Description: 
+ *
+ */
+
+get_header();
+
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main"></main>
+	</div>
+
+<?php get_footer(); ?>
