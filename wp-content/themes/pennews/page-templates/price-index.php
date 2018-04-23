c064eaaf741baf310d2eb328e731c0d8ce51078c
Leo Ordonez III <lordonez@ad.acquireap>
2018-04-23 16:08:42 +0800

    update price index page template

--- wp-content/themes/pennews/page-templates/price-index.php
+++ wp-content/themes/pennews/page-templates/price-index.php
@@ -9,12 +9,24 @@ get_header();
 
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-			<header><?php echo get_the_title(); ?></header>
-
-
-		</main>
-	</div>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
+		<div class="penci-container">
+			<div class="penci-container__content">
+				<div class="penci-wide-content penci-sticky-content">
+					<div class="penci-content-post">
+						<article>
+							<div class="wpb_text_column wpb_content_element ">
+								<div class="wpb_wrapper">
+									<h1><?php get_the_title() ?></h1>
+								</div>
+							</div>
+						</article>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+</div>
 
 <?php get_footer(); ?>
