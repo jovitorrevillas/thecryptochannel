633b06e4f276e9f05af9353bbd37380fd99fd864
Leo Ordonez III <lordonez@ad.acquireap>
2018-04-23 16:39:11 +0800

    added dummy text for styling

--- wp-content/themes/pennews/page-templates/price-index.php
+++ wp-content/themes/pennews/page-templates/price-index.php
@@ -20,6 +20,46 @@ get_header();
 								<div class="wpb_wrapper">
 									<h1><?php echo get_the_title(); ?></h1>
 									<div class="price-index-summary-column col-md-9">
+										<div class="row">
+											<div class="col-md-4">
+											</div>
+											<div class="col-md-4">
+												<div class="field">
+													<label>Open 24H</label>
+													<span>$ 8 937.95</span>
+												</div>
+												<div class="field">
+													<label>High 24H</label>
+													<span>$ 9 137.69</span>
+												</div>
+												<div class="field">
+													<label>Low 24H</label>
+													<span>$ 8 773.66</span>
+												</div>
+												<div class="field">
+													<label>Last Price</label>
+													<span>$ 8 954.06</span>
+												</div>
+											</div>
+											<div class="col-md-4">
+												<div class="field">
+													<label>Total</label>
+													<span>Ƀ 16 945 912.00</span>
+												</div>
+												<div class="field">
+													<label>Mkt. Cap</label>
+													<span>$ 151B</span>
+												</div>
+												<div class="field">
+													<label>Vol 24H</label>
+													<span>Ƀ 61 919.49</span>
+												</div>
+												<div class="field">
+													<label>Vol.24H</label>
+													<span>$ 552 026 293.53</span>
+												</div>
+											</div>
+										</div>
 									</div>
 									<div class="col-md-3">
 									</div>
