3b302663de379d5db1c38371a274d45d2babf615
Leo Ordonez III <lordonez@ad.acquireap>
2018-04-24 12:32:21 +0800

    update currency selector

--- wp-content/themes/pennews/page-templates/price-index.php
+++ wp-content/themes/pennews/page-templates/price-index.php
@@ -138,6 +138,7 @@ $symbol = $symbols[get_the_ID()];
 	    var buttons = document.querySelectorAll('li.currency-item');
 		[].forEach.call(buttons, function(el) {
 			el.addEventListener('click', function(e) {
+				console.log(e);
 				e.target.parentElement.className += ' active';
 				toggleCurrency(e.target.innerHTML);
 			});
