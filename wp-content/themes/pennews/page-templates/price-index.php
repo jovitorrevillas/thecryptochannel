9045c899f2f2916533d267c092b89c77078fbad0
Leo Ordonez III <lordonez@ad.acquireap>
2018-04-26 11:39:07 +0800

    update currencies

--- wp-content/themes/pennews/page-templates/price-index.php
+++ wp-content/themes/pennews/page-templates/price-index.php
@@ -56,14 +56,21 @@ $currenciesRaw = $symbols[get_the_ID()]['currencies'];
 	// CONFIG
 	var GLOBAL_SYMBOL = <?php echo "\"$symbol\""; ?>;
 	var GLOBAL_CURRENCIES = <?php echo "\"$currencies\""; ?>;
+	var GLOBAL_CURRENCIES_RAW = <?php echo "\"$currenciesRaw\""; ?>;
+	console.log(GLOBAL_CURRENCIES_RAW);
 	var GLOBAL_CURRENCY_SYMBOLS = {"USD":"$","EUR":"€","GBP":"£","JPY":"¥","RUR":"₽"};
 	var GLOBAL_CURRENCY_EXCHANGES = {
 		'BTC': {"USD":["Index","Bitfinex","GDAX","Bitstamp","Gemini","BitTrex","Kraken","HitBTC"],"EUR":["Index","Kraken","Bitstamp","GDAX","Gatecoin","Exmo","Quoine"],"GBP":["Index","Coinfloor","LakeBTC","GDAX","Localbitcoins","Kraken"],"CNY":["Index","OKCoin CNY","Huobi","Localbitcoins"],"JPY":["Index","Coincheck","Zaif","Quoine","LakeBTC"],"RUR":["Index","Livecoin","Exmo"]},
+
 		'BCH': {"USD":["Index","Bitfinex","BitTrex","HitBTC","Kraken","Poloniex"],"EUR":["Index","Kraken"],"GBP":["Index","Coinfloor"],"CNY":["Index","OKCoin CNY","Bter"],"JPY":["Index","Zaif","Quoine"],"RUR":["Index","Livecoin"]},
+
 		'ETH': {"USD":["Index","Bitfinex","HitBTC","GDAX","Kraken","Gemini","Poloniex"],"EUR":["Index","GDAX","Bitstamp","Kraken","Gatecoin","Exmo","HitBTC"],"GBP":["Index","Kraken"],"CNY":["Index","OKCoin CNY"],"JPY":["Index","Quoine","Kraken"],"RUR":["Index","YoBit","Exmo","Livecoin"]},
-		'LTC': {"USD":["Index","Bitfinex","HitBTC","GDAX","Kraken","Gemini","Poloniex"],"EUR":["Index","GDAX","Bitstamp","Kraken","Gatecoin","Exmo","HitBTC"],"GBP":["Index","Kraken"],"CNY":["Index","OKCoin CNY"],"JPY":["Index","Quoine","Kraken"],"RUR":["Index","YoBit","Exmo","Livecoin"]},
-		'XRP': {"USD":["Index","Bitfinex","HitBTC","GDAX","Kraken","Gemini","Poloniex"],"EUR":["Index","GDAX","Bitstamp","Kraken","Gatecoin","Exmo","HitBTC"],"GBP":["Index","Kraken"],"CNY":["Index","OKCoin CNY"],"JPY":["Index","Quoine","Kraken"],"RUR":["Index","YoBit","Exmo","Livecoin"]},
-		'XMR': {"USD":["Index","Bitfinex","HitBTC","GDAX","Kraken","Gemini","Poloniex"],"EUR":["Index","GDAX","Bitstamp","Kraken","Gatecoin","Exmo","HitBTC"],"GBP":["Index","Kraken"],"CNY":["Index","OKCoin CNY"],"JPY":["Index","Quoine","Kraken"],"RUR":["Index","YoBit","Exmo","Livecoin"]},
+
+		'LTC': {"USD":["Index","Bitfinex","HitBTC","GDAX","Kraken"],"EUR":["Index","GDAX","Bitstamp","Kraken"],"GBP":["Index"],"CNY":["Index","OKCoin CNY"],"JPY":["Index"],"RUR":["Index"]},
+
+		'XRP': {"USD":["Index","Bitfinex","HitBTC","Kraken","Poloniex"],"EUR":["Index"],"GBP":["Index"],"CNY":["Index","OKCoin CNY"],"JPY":["Index"]},
+
+		'XMR': {"USD":["Index","Bitfinex","HitBTC","Kraken","Poloniex"],"EUR":["Index","Kraken","Exmo"],"GBP":["Index"],"CNY":["Index","OKCoin CNY"],"RUR":["Index"]},
 	};
 
 	document.addEventListener("DOMContentLoaded", function(event) { 
