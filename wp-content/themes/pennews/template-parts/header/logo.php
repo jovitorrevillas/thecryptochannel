6d8eb08807650f567ad860251fe7ff3db9d86228
Jovi <jovi.to@ibinex.tech>
2018-05-16 03:15:08 +0000

    Replaced logo and made it static

--- wp-content/themes/pennews/template-parts/header/logo.php
+++ wp-content/themes/pennews/template-parts/header/logo.php
@@ -1,16 +1,5 @@
 <div class="site-branding">
-	<?php if( penci_get_setting( 'penci_use_textlogo' ) && penci_get_setting( 'penci_text_logo' ) ) : ?>
-		<?php if ( is_front_page() ) : ?>
-		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" itemprop="url"><?php echo penci_get_setting( 'penci_text_logo' ); ?></a></h1>
-		<?php else : ?>
-		<div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" itemprop="url"><?php echo penci_get_setting( 'penci_text_logo' ); ?></a></div>
-		<?php endif; ?>
-	<?php elseif ( function_exists( 'the_custom_logo' ) && has_custom_logo() ) : ?>
-		<?php the_custom_logo(); ?>
-	<?php endif; ?>
-	<?php
-	$description = penci_get_setting( 'penci_header_slogan_text' );
-	if ( $description ) : ?>
-		<span class="site-description"><?php echo esc_html( $description ); ?></span>
-	<?php endif; ?>
-</div><!-- .site-branding -->
+	<a href="http://thecryptochannel.tech/" class="custom-logo-link" rel="home" itemprop="url">
+		<img src="<?php echo get_template_directory_uri() . '/images/crypto-logo.png'; ?>" class="custom-logo" alt="TheCryptoCurrency" itemprop="image" width="1000" height="712">
+	</a>
+</div>
